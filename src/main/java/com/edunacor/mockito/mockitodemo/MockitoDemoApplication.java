008181b1d6567b5a466cbab4d2f761137b547b32
Amitabh Verma <ver_amitabh@yahoo.com>
2018-08-20 21:21:22 +0530

    2nd push

--- src/main/java/com/edunacor/mockito/mockitodemo/MockitoDemoApplication.java
+++ src/main/java/com/edunacor/mockito/mockitodemo/MockitoDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MockitoDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Start Program");
 		SpringApplication.run(MockitoDemoApplication.class, args);
 	}
 }
